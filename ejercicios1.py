9ce9757b2508d8ab89c3c46d1a09548a991146d4
Max Zeballos <max.zeballos@rankmi.com>
2021-05-19 22:27:18 -0500

    add ejercicios

--- ejercicios1.py
+++ ejercicios1.py
@@ -1,7 +1,12 @@
 # ejercicio 1
 
-age = int(input("Cual es tu edad: "))
-if age < 18:
-	print ("Eres menor de edad.")
-else:
-	print("Eres mayor de edad.")
+#age = int(input("Cual es tu edad: "))
+#if age < 18:
+#	print ("Eres menor de edad.")
+#else:
+#	print("Eres mayor de edad.")
+
+# Ejercicio 2
+
+#mensaje = "¡Hola Mundo!"
+#print(mensaje)
