1e1470dfeef81e28cf99a5e39a9a52690f1b873a
Max Zeballos <max.zeballos@rankmi.com>
2021-06-24 15:14:01 -0500

    add exercies

---
+++ python/ejercicios1.py
@@ -0,0 +1,32 @@
+def duplicar(numbers):
+	result = []
+	for number in numbers:
+		result.append(number*2)
+	return result
+
+def triplicar(numbers):
+	return
+
+def esPar(numbers):
+	return
+
+def multiply(numbers,n):
+	return
+
+def esPrimo(numbers):
+	return 
+
+#numberlist = [2,5,8,10]
+#print(duplicar(numberlist))
+
+#numberlist = [2,5,8,10]
+#print(triplicar(numberlist))
+
+#numberlist = [2,5,8,10]
+#print(esPar(numberlist)) # [true, false, true,true]
+
+#numberlist = [2,5,8,10]
+#print(multiply(numberlist,4)) # [8, 20, 32, 40]
+
+#numberlist = [1,5,10]
+#print(esPrimo(numberlist,4)) # [true, true, false]
