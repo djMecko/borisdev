3fa6973fea6274896fef0efd2ee643a1d7defbac
Max Zeballos <max.zeballos@rankmi.com>
2021-06-26 21:41:00 -0500

    add components

---
+++ python/ejercicios2.py
@@ -0,0 +1,14 @@
+def concat(lista1, lista2) # [1,2,3], [2,3] => [1,2,3,2,3]
+	return 
+
+def find(lista, number) # [1,2,4,6], 8 => false, [1,2,4,8], 8 => true
+	return
+
+def counter(lista, number) # [1,2,1,6], 1 => 2, [1,2,1,6], 2 => 1 
+	return
+
+def power(lista, number) # [1,2,1,6], 2 => [1,4,1,36]
+	return
+
+def twocomponents(lista, number) # [1,2,1,6], 8 => [6,2] | # [1,2,1,6], 9 => []
+	return
