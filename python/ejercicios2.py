39917a780acf7d2cf70d677626fc9224c7a70f7e
Max Zeballos <max.zeballos@rankmi.com>
2021-06-27 22:00:51 -0500

    add exercices

--- python/ejercicios2.py
+++ python/ejercicios2.py
@@ -1,14 +1,21 @@
-def concat(lista1, lista2) # [1,2,3], [2,3] => [1,2,3,2,3]
-	return 
+def concat(lista1, lista2):  # [1,2,3], [2,3] => [1,2,3,2,3]
+	return lista1 + lista2
 
-def find(lista, number) # [1,2,4,6], 8 => false, [1,2,4,8], 8 => true
+def find(lista, number): # [1,2,4,6], 8 => false, [1,2,4,8], 8 => true
 	return
 
-def counter(lista, number) # [1,2,1,6], 1 => 2, [1,2,1,6], 2 => 1 
+def counter(lista, number): # [1,2,1,6], 1 => 2, [1,2,1,6], 2 => 1 
 	return
 
-def power(lista, number) # [1,2,1,6], 2 => [1,4,1,36]
+def power(lista, number): # [1,2,1,6], 2 => [1,4,1,36]
 	return
 
-def twocomponents(lista, number) # [1,2,1,6], 8 => [6,2] | # [1,2,1,6], 9 => []
-	return
+
+assert(concat([1,2,3],[4,5,6]) == [1,2,3,4,5,6])
+
+assert(find([1,2,4,6],8) == False)
+assert(find([1,2,4,8],8) == True)
+
+assert(counter([1,2,1,6],1) == 2)
+
+assert(power([1,2,1,6],2) == [1,4,1,36])
