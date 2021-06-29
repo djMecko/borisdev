f0dc31c2b36cf0a4a84a3de9d12d188c6ecadd6a
Max Zeballos <max.zeballos@rankmi.com>
2021-06-28 21:54:54 -0500

    add results

--- python/ejercicios2.py
+++ python/ejercicios2.py
@@ -1,21 +1,31 @@
-def concat(lista1, lista2):  # [1,2,3], [2,3] => [1,2,3,2,3]
-	return lista1 + lista2
+def concat(l1, l2):  # [1,2,3], [2,3] => [1,2,3,2,3]
+	return l1 + l2
 
-def find(lista, number): # [1,2,4,6], 8 => false, [1,2,4,8], 8 => true
-	return
+def findList(l, n): # [1,2,4,6], 8 => false, [1,2,4,8], 8 => true
+	return n in l
 
 def counter(lista, number): # [1,2,1,6], 1 => 2, [1,2,1,6], 2 => 1 
-	return
+	count = 0;
+	for item in lista:
+		if item == number:
+			count += 1
+	return count
 
 def power(lista, number): # [1,2,1,6], 2 => [1,4,1,36]
-	return
+	return [pow(item, number) for item in lista]
 
+def power2(lista, number):
+	result = []
+	for item in lista:
+		result.append(item**number)
+	return result
 
 assert(concat([1,2,3],[4,5,6]) == [1,2,3,4,5,6])
 
-assert(find([1,2,4,6],8) == False)
-assert(find([1,2,4,8],8) == True)
+assert(findList([1,2,4,6],8) == False)
+assert(findList([1,2,4,8],8) == True)
 
 assert(counter([1,2,1,6],1) == 2)
 
 assert(power([1,2,1,6],2) == [1,4,1,36])
+assert(power2([1,2,1,6],2) == [1,4,1,36])
