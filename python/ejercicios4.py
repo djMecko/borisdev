6e8b137b0e875362aa58950dbc2ebd1fa10a76d3
Max Zeballos <max.zeballos@rankmi.com>
2021-07-02 12:15:44 -0500

    solutions

--- python/ejercicios4.py
+++ python/ejercicios4.py
@@ -1,27 +1,32 @@
 # add n to elements of the list
 def increment(lista, n):
-	return
+	return [item + n for item in lista]
 
 # substract n to elements of the list
 def decrement(lista, n):
-	return
+	return [item - n for item in lista]
 
 # Reduce the size of list to the first n elements
 def cutlist(lista, firstn):
-	return
+	return lista[:firstn]
 
 # Sum in increments
 # example1: [1,2,3] => [1, 1+2, 1+2+3] => [1,2,6]
 # example2: [1,3,9] => [1, 1+3, 1+3+9] => [1,4,13]
 def incrementalsum(lista):
-	return
+	newlist = []
+	counter = 0
+	for item in lista:
+		counter += item
+		newlist.append(counter)
+	return newlist
 
 assert(increment([2,3,4],1) == [3,4,5])
 
 assert(decrement([2,3,4],1) == [1,2,3])
 
-assert(decrement([2,3,4,5],2) == [2,3])
-assert(decrement([2,3,4,5],3) == [2,3,4])
+assert(cutlist([2,3,4,5],2) == [2,3])
+assert(cutlist([2,3,4,5],3) == [2,3,4])
 
-assert(incrementalsum([1,2,3]) == [1,2,6])
+assert(incrementalsum([1,2,3]) == [1,3,6])
 assert(incrementalsum([1,3,9]) == [1,4,13])
