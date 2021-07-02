fd6ab72532c8204c8ef810c9297f593800cce0c6
Max Zeballos <max.zeballos@rankmi.com>
2021-07-02 12:20:28 -0500

    add ejercicios

---
+++ python/ejercicio5.py
@@ -0,0 +1,19 @@
+# Convet to upper case
+def toUpper(word):
+	return 
+
+# Convert to lower case
+def toLower(word):
+	return 
+
+# Return true if the word contain certain letter
+def containLetter(word, leter):
+	return
+
+assert(toUpper("Max") == "MAX")
+assert(toUpper("Puppy") == "PUPPY")
+
+assert(toLower("Puppy") == "puppy")
+
+assert(containLetter("cancha", "a") == True)
+assert(containLetter("cancha", "z") == False)
