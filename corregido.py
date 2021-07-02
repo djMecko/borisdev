904d94c02d305b6fbe27d529e5627f99c26ffd9e
fabio fei <feiqiangli@hotmail.com>
2021-07-01 20:29:33 -0500

    corregidos

---
+++ corregido.py
@@ -0,0 +1,32 @@
+# ejercicio 1
+numeros = [1,2,3]
+n=int(input("ingrese un valor entero"))
+for i in range(len(numeros)): 
+    numeros[i] += n
+print(numeros)
+
+#ejercicio 2
+lista=[1,2,3,4,5]
+n=int(input("ingrese un valor a restar ala lista"))
+for i in range(len(lista)):
+    lista[i] -= n
+print(lista)
+
+# ejercicio 3
+def pri(lista,n):
+    return(p[:n])
+print(pri([1,2,3,4],2))
+
+def pri(lista,n):
+    return(lista[:n])
+print(pri([1,2,3,4,5,6,7,8,9],5))
+
+
+
+p=[1,2,3,4,5]
+n=int(input("ingrese el valor para cortar la lista"))
+print(p[:n])
+
+#ejercicio 4 # no intente mash
+
+
