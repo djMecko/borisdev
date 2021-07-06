e30d822dc7aa3fec2afe3644f317bff2aec6de9f
Max Zeballos <max.zeballos@rankmi.com>
2021-07-05 21:30:17 -0500

    add ejercicios

---
+++ python/ejercicios6.py
@@ -0,0 +1,32 @@
+# [1, 2, 3] =>  1 + 4 + 9 => 14
+def sumaOfSquares(lista):
+	return
+
+# (1, 2, 5) => (x, -x)
+def generalFormule(a, b, c):
+	return 
+
+# [1, 3, 4, 6] => 1 - 3 + 4 - 6 => -4
+def sumAndSubstract(lista):
+	return
+
+# [1, 2, 2, 1] => True
+# [1, 2, 3, 1] => False
+def isPalindromo(lista):
+	return
+
+# [0,0,0,0,0,0] => True
+# [0,0,0,0,1,0] => False
+def isZeros(lista):
+	return
+
+# [1, 2, 1, 1, 1] => False
+# [2, 2, 2, 2, 2] => True
+def areEquals(lista):
+	return
+
+# ("camita", "cam") => True
+# ("camita", "ita") => True
+# ("camita", "mta") => False
+def isSubString(word, substring):
+	return
