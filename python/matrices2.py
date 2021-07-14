26db7a5d23f6bac71707460aba98b4fff3cecf3c
Max Zeballos <max.zeballos@rankmi.com>
2021-07-14 07:53:39 -0500

    add matrices

---
+++ python/matrices2.py
@@ -0,0 +1,17 @@
+# Add one scalar to matrix A + a = B
+# ([[1,1],[1,1]], 2) => [[3, 3],[3 ,3]]
+# OJO: que solo es 1 matris mas 1 escalar, no dos matrices mas un escalar
+def addScalar(mat, number):
+    return 
+
+# Return true if two matrix are equal, in other case return false
+def areEqual(mat1, mat2):
+    return
+
+# Return true if all elements of matrix is 0
+def equaltozero(mat1):
+    return
+
+# Return true if all values of matrix are equals, in other case return false
+def allElementsAreEquals(mat1):
+    return 
