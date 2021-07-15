cd8150b8c8eadda75e9126bc562cde1007288c4d
Max Zeballos <max.zeballos@rankmi.com>
2021-07-15 00:48:42 -0500

    add matrices

---
+++ python/matrices3.py
@@ -0,0 +1,20 @@
+# This function return a matrix multiplied by an scalar
+# ([[1,2],[1,3]], 3) => [[3,6],[3,9]]
+def multiplyByScalar(mat, number):
+	return 
+
+# Sum all elements of matrix and return this result
+# [[1,2],[1,3]] => 7
+def sumAllElements(mat):
+	return
+
+# Sum the diagonal of matrix
+# [[1,2],[1,3]] => 1 + 3 = 7
+def sumDiagonal(mat):
+	return
+
+# Find an element in the matrix
+# [[1,2],[1,3]], 3) => True
+# [[1,2],[1,3]], 4) => False
+def findElement(mat, number):
+	return
